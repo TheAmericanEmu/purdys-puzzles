5f6ff4da4f8517600dcf254738393ecf60b9dc1d
TheAmericanEmu <102053701+TheAmericanEmu@users.noreply.github.com>
2023-11-01 09:50:16 -0500

    Update mySlove.py

--- Puzzles/2023-11/mySlove.py
+++ Puzzles/2023-11/mySlove.py
@@ -1,1 +1,23 @@
+from string import ascii_lowercase as alp
+coded = "jyyqd://yomuita.vpz/4bcs5jle"
 
+def grab_letter(letter:str)-> int:
+  count = 0
+  if letter == "/" or letter == "." or letter ==":":
+    return None
+  for char in alp:
+    if letter = char:
+      return count
+    count+=1
+    
+    
+key = 6
+newString=""
+for code_leter in coded:
+  alp_spot = grab_letter(code_leter)
+  if alp_spot !=None:
+    newString=newString+alp[(alp_spot-1)+key]
+  if alp_spot==None:
+    newString=newString+coded_letter
+    
+    
