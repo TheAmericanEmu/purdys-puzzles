f5b3a02059ece6811d66629e3726ea24373aa0db
TheAmericanEmu <102053701+TheAmericanEmu@users.noreply.github.com>
2023-11-01 11:46:10 -0500

    Add files via upload

--- Puzzles/2023-11/mySlove.py
+++ Puzzles/2023-11/mySlove.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase as alp
+
 coded = "jyyqd://yomuita.vpz/4bcs5jle"
 
 def grab_letter(letter:str)-> int:
@@ -9,16 +10,41 @@ def grab_letter(letter:str)-> int:
     if letter == char:
       return count
     count+=1
-    
-    
-key = 6
-newString=""
-for code_leter in coded:
-  alp_spot = grab_letter(code_leter)
-  if alp_spot !=None:
-    newString=newString+alp[(alp_spot-1)+key]
-  if alp_spot==None:
-    newString=newString+coded_letter
 
-print(newString)
+
     
+key1=-3
+key2=-7
+key3=-7
+key4=-3
+key5=15
+keys=[-3,-7,-7,-3,15]
+newString=""
+five_spilt=[]
+count = 0
+spilt = ""
+for char in coded:
+  count+=1
+  
+  spilt=spilt+char
+  print(count)
+  if count == 5:
+    five_spilt.append(spilt)
+    spilt=""
+    count=0
+for code_letter in coded:
+  alp_spot = grab_letter(code_letter)
+  
+  ##  if alp_spot !=None:
+  ##    if (alp_spot)+key >=26:
+  ##      alp_spot=(26-((alp_spot-1)+key))*-1
+  ##      print(alp_spot)
+  ##    else:
+  ##      alp_spot=key+(alp_spot-1)
+  ##    newString=newString+alp[alp_spot]
+  ##  if alp_spot==None:
+  ##    newString=newString+code_letter
+  ##coded=newString
+print(five_spilt)
+    
+print("Done")
