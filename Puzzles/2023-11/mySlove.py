3e2eaab86b61842fdc4e6471f6e7f6b8dbd4fee0
TheAmericanEmu <thegreatemu02@gmail.com>
2023-11-01 14:23:48 -0500

    Update mySlove.py

--- Puzzles/2023-11/mySlove.py
+++ Puzzles/2023-11/mySlove.py
@@ -1,7 +1,8 @@
+
 from string import ascii_lowercase as alp
 
 coded = "jyyqd://yomuita.vpz/4bcs5jle"
-
+#https://youtube.com/4bcs5jle
 def grab_letter(letter:str)-> int:
   count = 0
   if letter == "/" or letter == "." or letter ==":":
@@ -12,39 +13,40 @@ def grab_letter(letter:str)-> int:
     count+=1
 
 
+def letter_decode(letter,key):
+  alp_spot = grab_letter(letter)
+  if alp_spot !=None:
+    if (alp_spot)+key >=26:
+      alp_spot=(26-((alp_spot-1)+key))*-1
+      print(alp_spot)
+    else:
+      alp_spot=key+(alp_spot-1)
+    return(alp[alp_spot])
+  return(letter)
+
     
-key1=-3
-key2=-7
-key3=-7
-key4=-3
-key5=15
-keys=[-3,-7,-7,-3,15]
+
+keys=[25,22,22,26,16]
 newString=""
 five_spilt=[]
 count = 0
 spilt = ""
+new_url = ""
 for char in coded:
-  count+=1
   
   spilt=spilt+char
+  count+=1
   print(count)
   if count == 5:
     five_spilt.append(spilt)
     spilt=""
     count=0
-for code_letter in coded:
-  alp_spot = grab_letter(code_letter)
-  
-  ##  if alp_spot !=None:
-  ##    if (alp_spot)+key >=26:
-  ##      alp_spot=(26-((alp_spot-1)+key))*-1
-  ##      print(alp_spot)
-  ##    else:
-  ##      alp_spot=key+(alp_spot-1)
-  ##    newString=newString+alp[alp_spot]
-  ##  if alp_spot==None:
-  ##    newString=newString+code_letter
-  ##coded=newString
-print(five_spilt)
-    
+five_spilt.append(spilt)
+for group in five_spilt:
+  for i in range(len(group)):
+    char = group[i]
+    key = keys[i]
+    new_url= new_url+letter_decode(char,key)
+
+print(new_url)    
 print("Done")
