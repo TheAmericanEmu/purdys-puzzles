aee11bddd8ff83a2028c060bcb18de9242c90928
TheAmericanEmu <102053701+TheAmericanEmu@users.noreply.github.com>
2023-11-02 11:11:46 -0500

    Update mySlovenew.py

--- Puzzles/2023-11/mySlovenew.py
+++ Puzzles/2023-11/mySlovenew.py
@@ -1,7 +1,10 @@
-code = "jyyqd://yomuita.vpz/4bcs5jle"
+code = ""
 qwerty_dic = {}
 num_to_qarty = {}
 
+with open("C:\\Users\\Certiport\\Downloads\\thing.txt") as file:
+    for line in file:
+        code=code+line.strip()
 def make_dics():
     keybroad ="qwertyuiopasdfghjklzxcvbnm"
     for i in range(len(keybroad)):
@@ -22,8 +25,18 @@ for char in code:
             key =0
         else:
             key-=1
-        new_url.append(num_to_qarty[key])
+        letter = None
+        if num_to_qarty[key] == "l" and num_to_qarty[key+1] == "z":
+            letter="m"
+        elif num_to_qarty[key] == "p" and num_to_qarty[key+1] == "a":
+            letter = "l"
+        elif num_to_qarty[key+1] == "q":
+            letter = "p"
+        if letter == None:
+            letter = num_to_qarty[key]
+        new_url.append(letter)
     else:
         new_url.append(char)
 
 print("".join(new_url))
+
