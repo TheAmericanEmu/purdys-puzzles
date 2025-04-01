7dc45a44de2b965ffb70949f2559e016f7ce7773
TheAmericanEmu <102053701+TheAmericanEmu@users.noreply.github.com>
2025-04-01 10:59:35 -0500

    Update Solver.java

--- Puzzles/2025-04/Solver.java
+++ Puzzles/2025-04/Solver.java
@@ -3,24 +3,65 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 
-public class Solver{
-    public static void main(String[] args) throws Exception{
+public class MyProgram
+{
+
+        public static int convertArrayToInt(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num);
+        }
+
         try {
-            int[][] ints = new int[100][100];
+            return Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+             throw new NumberFormatException("Could not parse the combined string to an integer: " + sb.toString());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[][] ints = new int[100][100];
+        try {
             File file = new File("april25_puzzle.txt");
             Scanner input = new Scanner(file);
             int count=0;
             while(input.hasNextLine()){
                 String line =input.nextLine();
-                for(int i=0;i<line.length()-1;i++){
-                    ints[count][i]=(Integer.numberOf(line.substring(i,i+1)));
+                for(int i=0;i<=line.length()-1;i++){
+                    ints[count][i]=(Integer.valueOf(line.substring(i,i+1)));
                 }
                 count++;
             }
-            System.out.println(ints[0].length);
+
         } catch (Exception e) {
             System.out.println(e);
         }
+        ArrayList<Integer> workingInts= new ArrayList<Integer>();
+        for(int[] row: ints){
+            for(int num: row){
+                System.out.print(num);
 
+            }
+            
+            System.out.println("-------------------");
+            System.out.println(convertArrayToInt(row));
+    
+        }
+
+        for(int i3=100;i3<201;i3++){
+            for(int row=0;row<ints.length;row++){
+                
+                if(convertArrayToInt(ints[row])%i3==0){
+                    workingInts.add(i3);
+                }
+            }
+        }
+        for(int num: workingInts){
+            System.out.println(num);
+        }
     }
 }
