e0b13c1f48a05640ae33e21a790e50acaec8e884
colin <Thegreatemu02@gmail.com>
2025-04-02 09:40:05 -0500

    fun

--- Puzzles/2025-04/Solver.java
+++ Puzzles/2025-04/Solver.java
@@ -1,12 +1,36 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.math.BigInteger;
 
 
-public class MyProgram
+public class Solver
 {
-
-        public static int convertArrayToInt(int[] arr) {
+        //o calculate all common divisors 
+        // of two given numbers 
+        // a, b --> input integer numbers 
+        static int commDiv(BigInteger a, BigInteger b) 
+        { 
+            // find gcd of a, b 
+            BigInteger n = a.gcd(b); 
+    
+            // Count divisors of n. 
+            int result = 0; 
+            for (BigInteger i = new BigInteger("1"); i.compareTo(n.sqrt())<1; i.add(new BigInteger("1"))) { 
+                // if 'i' is factor of n 
+                //System.out.println(i);
+                if (n.mod(i).equals(new BigInteger("0"))) { 
+                    // check if divisors are equal 
+                    if (n.divide(i).equals(i)) 
+                        result += 1; 
+                    else
+                        result += 2; 
+                } 
+            } 
+            return result; 
+        } 
+ 
+        public static BigInteger convertArrayToInt(int[] arr) {
         if (arr == null || arr.length == 0) {
             throw new IllegalArgumentException("Array cannot be null or empty");
         }
@@ -17,11 +41,11 @@ public class MyProgram
         }
 
         try {
-            return Integer.parseInt(sb.toString());
+            return new BigInteger(sb.toString());
         } catch (NumberFormatException e) {
              throw new NumberFormatException("Could not parse the combined string to an integer: " + sb.toString());
         }
-    }
+        }   
 
     public static void main(String[] args) throws Exception{
         int[][] ints = new int[100][100];
@@ -43,24 +67,32 @@ public class MyProgram
         ArrayList<Integer> workingInts= new ArrayList<Integer>();
         for(int[] row: ints){
             for(int num: row){
-                System.out.print(num);
+                // System.out.print(num);
 
             }
             
-            System.out.println("-------------------");
-            System.out.println(convertArrayToInt(row));
+            // System.out.println("-------------------");
+            // System.out.println(convertArrayToInt(row));
     
         }
 
-        for(int i3=100;i3<201;i3++){
-            for(int row=0;row<ints.length;row++){
-                
-                if(convertArrayToInt(ints[row])%i3==0){
-                    workingInts.add(i3);
+        //for(int i3=100;i3<1000;i3++){
+            
+        for(int row=0;row<ints.length;row++){
+            BigInteger a = convertArrayToInt(ints[row]);
+            //System.out.println(newLcm);
+            for(int row2=0;row2<ints.length;row2++){
+                BigInteger b = convertArrayToInt(ints[row2]);
+                int newLcm=commDiv(a,b);
+                //System.out.println(newLcm);
+                if(true){
+                    workingInts.add(newLcm);
+                    //System.out.println(":"+newLcm);
                 }
             }
         }
-        for(int num: workingInts){
+        //}
+        for(Integer num: workingInts){
             System.out.println(num);
         }
     }
